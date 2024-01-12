7bc598baf7191e3ca998130cb12df61e379fb777
JoAzar <joel.zalzar.trabajo@gmail.com>
2024-01-12 17:56:25 -0300

    Agregado de botones en todas las rutas y vídeos en Wireshark

--- funcionalidades.js
+++ funcionalidades.js
@@ -1,4 +1,5 @@
 
+/*
 function alerta() {
   alert("ESTÁS USANDO JS");
 }
@@ -115,6 +116,7 @@ function email(event) {
 
 padre4.addEventListener('click', email);
 
+*/
 //--------------------------------------------------------/
 
 function cambiarIconoMono() {
@@ -127,5 +129,31 @@ function restaurarIconoMono() {
   mono.src = "icons/mono.png";
 }
 
-/*--------------------VENTANAS EMERGENTES-----------------------------------*/
+/*--------------------BOTÓN SUBIR-----------------------------------*/
+const scrollStep = 20;
 
+function scrollUp() {
+  const currentScroll = window.scrollY;
+  if(currentScroll > 0) {
+    window.requestAnimationFrame(scrollUp);
+    window.scrollTo(0, Math.max(0, currentScroll - scrollStep));
+  }
+}
+
+function scrollDown() {
+  const currentScroll = window.scrollY;
+  const windowHeight = window.innerHeight;
+  const documentHeight = Math.max(
+    document.body.scrollHeight, document.documentElement.scrollHeight,
+    document.body.offsetHeight, document.documentElement.offsetHeight,
+    document.body.clientHeight, document.documentElement.clientHeight
+  );
+
+  if (currentScroll + windowHeight < documentHeight) {
+    window.requestAnimationFrame(scrollDown);
+    window.scrollTo(0, currentScroll + Math.min(scrollStep, documentHeight - currentScroll - windowHeight));
+  }
+}
+
+document.getElementById("subir").addEventListener("click",scrollUp);
+document.getElementById("bajar").addEventListener("click",scrollDown);
