707a25320bc2f2da15aa18b5bf68c52b594b2c26
red <red@Skynet.Skynet>
2024-12-01 02:15:04 -0300

    cambios 1/Dic

--- README.md
+++ README.md
@@ -22,7 +22,10 @@
     <li>6. <del>Reparar la conexión con el servidor</del></li>
     <li>7. <del>Agregar landing page</del>del></li>
     <li>8. <del>Construir un nuevo sistema de navegación</del></li>
-    <li>9. Reparar todas las url y el sistema de navenación</li>
+    <li>9. <del>Reparar todas las url y el sistema de navenación</del></li>
     <li>10. Agregar más contenido</li>
+    <li>11. Cambiar el nav de formulario para que sólo vuelva a inicio</li>
+    <li>12. Cambiar el diseño de botónes de subir y bajar</li>
+    <li>13. Modificar toda la tienda</li>
 </ul>
 <img src="https://user-images.githubusercontent.com/73097560/115834477-dbab4500-a447-11eb-908a-139a6edaec5c.gif">
--- routes.js
+++ routes.js
@@ -96,3 +96,57 @@ fetch('../routes.json')
             innerDiv.appendChild(p);
         });
     })
+
+fetch('../routes.json')
+    .then(response => response.json())
+    .then(data => {
+        const contenedorHardware = document.getElementById('contenedor-hardware');
+        data.hardware.forEach(hardware => {
+            const div = document.createElement('div');
+            div.classList.add('margen1', 'radius', 'encuadrar', 'shadow-inset-center-hover', 'bordeAzulIzqHover', 'bordeAzulAbajoHover', 'growHover', 'manito');
+            div.onclick = () => {
+                location.href = hardware.url;
+            };
+            const innerDiv = document.createElement('div');
+            innerDiv.classList.add('letraBlanca', 'sombraNegra', 'textoLato', 'tamanioMed', 'padding2', 'radius');
+
+            const p = document.createElement('p');
+            p.classList.add('tamaniopeq', 'letraCeleste', 'bordeGrisArriba');
+            p.textContent = hardware.descripcion || 'Descripción no disponible';
+
+            const h1 = document.createElement('h1');
+            h1.classList.add('textoLato', 'letraBlanca', 'tamanioMed');
+            h1.textContent = hardware.nombre;
+            
+            innerDiv.appendChild(h1);
+            div.appendChild(innerDiv);
+            contenedorHardware.appendChild(div);
+            innerDiv.appendChild(p);
+        });
+    })
+
+
+    /*------------------------- RUTEO DE LAS RUTAS ABSOLUTAS -----------------------------*/
+
+function obtenerRuta(relativa) {
+    const currentPath = window.location.pathname;
+    const pathParts = currentPath.split("/");
+    const base = pathParts.slice(0, pathParts.length - 1).join("/");
+    return (base.endsWith("/") ? "" : "" ) + relativa;
+    }
+    
+fetch('../routesAbsolute.json')
+    .then(response => response.json())
+    .then(data => {
+        const menu = document.getElementById('navMenu');
+        data.links.forEach(link => {
+        const li = document.createElement('li');
+        const h1 = document.createElement('h1');
+        h1.className = 'nav1 textoLato tamaniopeq growHover';
+        h1.textContent = link.name;
+        h1.onclick = () => location.href = obtenerRuta(link.url);
+    
+        li.appendChild(h1);
+        menu.appendChild(li);
+        });
+    })
